769d4b32c05db5107ed94f58c4de2cf4c1eb708d
tuhin92 <tuhinalmamun71m@gmail.com>
2024-11-13 22:31:10 +0600

    remove outlet

--- src/Layout/Main_layout.jsx
+++ src/Layout/Main_layout.jsx
@@ -1,10 +1,8 @@
-import { Outlet } from 'react-router-dom';
 
 const Main_layout = () => {
     return (
         <div>
             <h1>Navbar</h1>
-            <Outlet></Outlet>
             <h2>Footer</h2>
         </div>
     );
