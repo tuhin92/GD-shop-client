512e87a4600d1102e11126a383f066551dc54d4e
tuhin92 <tuhinalmamun71m@gmail.com>
2024-11-15 14:22:01 +0600

    add navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const navlinks = <>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/products">Products</Link></li>
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/contact">Contact us</Link></li>
+    </>
   return (
     <div className="bg-base-300">
       <div className="navbar max-w-7xl mx-auto">
@@ -26,48 +33,14 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {navlinks}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Gadget Shop</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navlinks}
           </ul>
         </div>
         <div className="navbar-end">
