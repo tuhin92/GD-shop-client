087c9abeb91dbcb1cc2436e27c3950843a1fc244
tuhin92 <tuhinalmamun71m@gmail.com>
2024-11-15 11:24:05 +0600

    align navbar content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,25 @@ const Home = () => {
   return (
     <div>
       <Banner />
-      <div className="my-8">
-        <h1 className="mb-16 text-2xl font-bold text-center">
-          Feature Products
-        </h1>
-        <FeatureProducts />
-      </div>
+      <div className="max-w-7xl mx-auto">
+        <div className="my-8">
+          <h1 className="mb-16 text-2xl font-bold text-center">
+            Feature Products
+          </h1>
+          <FeatureProducts />
+        </div>
 
-      <div className="my-8">
-        <h1 className="mb-16 text-2xl font-bold text-center">User Review</h1>
-        <UserReview />
-      </div>
+        <div className="my-8">
+          <h1 className="mb-16 text-2xl font-bold text-center">User Review</h1>
+          <UserReview />
+        </div>
 
-      <div className="my-8">
-        <h1 className="mb-16 text-2xl font-bold text-center">Frequently Asked Questions</h1>
-        <Accordion/>
+        <div className="my-8">
+          <h1 className="mb-16 text-2xl font-bold text-center">
+            Frequently Asked Questions
+          </h1>
+          <Accordion />
+        </div>
       </div>
     </div>
   );
