311835e394ab6a81d52458f81e940207a04abb0f
tuhin92 <tuhinalmamun71m@gmail.com>
2024-11-15 14:24:12 +0600

    create products about and contact page

---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>This is product page</h2>
+        </div>
+    );
+};
+
+export default Products;<h2>This is product page</h2>
